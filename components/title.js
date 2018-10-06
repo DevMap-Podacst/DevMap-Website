9ac133b43703af28d89f4b15f8987001a8ff5dc6
cpt_Crunch <jasong84@gmail.com>
2018-10-05 21:02:17 -0500

    added media queries for title info

--- components/title.js
+++ components/title.js
@@ -1,6 +1,6 @@
 const Title = () => (
   <div className="page-InfoContainer">
-    <img src="../static/Dev-Map-Logo.png" className="heroImage" />
+    <img src="../static/Dev-Map-Logo_400x250.png" className="heroImage" />
 	<h1 className="title">Welcome to DevMap!</h1>
 	<p className="description">
 	The Podcast for Junior Developers and the cure for Imposter Syndrome
@@ -8,33 +8,50 @@ const Title = () => (
     <style jsx>{`
         .page-InfoContainer {
           display: grid;
-          grid-template-rows: repeat(3, 1fr);
+          grid-template-rows: minmax(100px, auto);
           grid-template-columns: repeat(4, 1fr);
           grid-template-areas:
             ". logo logo ." 
-            ". info info .";
+            ". title title ."
+			". description description . ";
           justify-content: center;
           align-content: center;
         }
+		@media (max-width: 414px) {
+			.page-InfoContainer{
+				grid-template-areas:
+				"logo logo logo logo" 
+				"title title title title"
+				"description description description description";
+			}
+		}
+
         .heroImage {
-          height: 300px;
+          height: auto;
+		  max-width: 400px;
           grid-area: logo;
           justify-self: center;
           align-self: center;
         }
+		@media (max-width: 414px) {
+			.heroImage {
+				max-width: 90vw;
+			}
+		}
         .title {
           margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
+          padding: 0;
+          font-size: 2.5rem;
+		  grid-area: title;
         }
         .title,
         .description {
           text-align: center;
           color: #ffffff;
-          grid-area: info;
         }
+		.description {
+			grid-area: description;
+		}
       `}</style>
   </div>
 )
