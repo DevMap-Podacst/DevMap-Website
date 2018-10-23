1cf7c442e5c17670f6c0d2115dbfac5ab7b1342b
Jason Gutierrez <jasong84@gmail.com>
2018-10-23 17:43:19 -0500

    list out hard coded episode details

--- components/episodes.js
+++ components/episodes.js
@@ -1,36 +1,34 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Thumbnail from '../components/pCastThumbnail'
-import { faPlayCircle, faPlay, faPause, faStepBackward, faStepForward, faVolumeUp,faVolumeDown, faVolumeOff } from '@fortawesome/free-solid-svg-icons'
-library.add(faPlayCircle, faPlay, faPause, faStepBackward, faStepForward, faVolumeUp, faVolumeDown, faVolumeOff)
+import Thumbnail from './MediaPlayer/Thumbnail'
+import MediaController from './MediaPlayer/MediaController'
+const episodesList =  [
+	{ 
+		"href": "https://anchor.fm/devmappodcast",
+		"title": "Episode---000",
+		"description": "lorem ipsum",
+	},
+	{ 
+		"href": "https://anchor.fm/devmappodcast",
+		"title": "Episode---001",
+		"description": "lorem ipsum",
+	},
+	{ 
+		"href": "https://anchor.fm/devmappodcast",
+		"title": "Episode---002",
+		"description": "lorem ipsum",
+	}   
+	]
 
 const Episodes = () => (
         <div className="row">
-			<div className="pcast-player">
-				<Thumbnail />
-				<div className="pcast-controls">
-					<button class="pcast-play">
-						<FontAwesomeIcon icon="play" size="lg" />
-					</button>
-					<button class="pcast-pause">
-						<FontAwesomeIcon icon="pause" size="lg" />
-					</button>
-					<button class="pcast-rewind">
-						<FontAwesomeIcon icon="step-backward" size="lg" />
-					</button>
-					<button class="pcast-speed">1x</button>
-					<button class="pcast-skip">
-						<FontAwesomeIcon icon="step-forward" size="lg" />
-					</button>
-
-					<span class="pcast-currenttime pcast-time">00:00</span>
-					<progress class="pcast-progress" value="0"></progress>
-					<span class="pcast-duration pcast-time">00:00</span>
-					<button class="pcast-mute">
-						<FontAwesomeIcon icon="volume-down" size="lg" />
-					</button>
+			{episodesList.map( ({title, description }) => (
+				<div className="pcast-player">
+					<Thumbnail />
+					<h2 className="pcast-title">{title}</h2>
+					<p className="pcast-details">{description}</p>
+					<MediaController/>
+					<audio src="{href}" />
 				</div>
-			</div>
+			))}
             <style jsx global>{`
               .row {
 				align-items: center;
@@ -41,22 +39,35 @@ const Episodes = () => (
 				max-width: 880px;
               }
               .pcast-player {
-				border: 1px solid #e1e1e1;
+				border-top: 1px solid #e1e1e1;
+				border-left: 1px solid #e1e1e1;
 				display: grid;
                 grid-template-rows: auto;
                 grid-template-columns: max-content 1fr;
                 grid-template-areas:
-                  "pcastThumbnail pcastPlayer";
+				  "Thumbnail Player"
+				  "Thumbnail Player";
 				padding: 0;
 				margin: 8px 18px;
 				background: white;
-                grid-area: pcastPlayer;
                 width: 100%;
-                max-width: 500px;
-                height: 70px;
+                max-width: 600px;
+                height: 110px;
 				justify-self: center;        
 			  }
-			  
+			  .pcast-title {
+				  grid-area: Player;
+				  justify-self: stretch;
+				  align-self: start;
+				  margin: 0;
+			  }
+			  .pcast-details {
+				  grid-area: Player;
+				  justify-self: stretch;
+				  align-self: center;
+				  margin: 0;
+			  }
+
         	`}</style>
         </div>
 )
