61249836253e163a42664e9537cbb25b7c6feed1
Cpt_Crunch <jasong84@gmail.com>
2019-03-14 19:40:08 -0500

    Added the 3 newest episodes.

--- components/episodes.js
+++ components/episodes.js
@@ -1,6 +1,10 @@
 
 const Episodes = () => (
           <div className="row">
+            {/* Paste the iframe link here from Anchor */}
+            <iframe src="https://anchor.fm/devmappodcast/embed/episodes/005---Junior-VS-Senior-Developers--Whats-the-DIFF-e387ed" className="card" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://anchor.fm/devmappodcast/embed/episodes/004---The-Big-Big-Jon-Interview----now-without-gluten-e2m5f2" className="card" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://anchor.fm/devmappodcast/embed/episodes/003---New-hair--new-job-e2hcrk" className="card" frameborder="0" scrolling="no"></iframe>
             <iframe src="https://anchor.fm/devmappodcast/embed/episodes/002---The-Technical-Interview-e2eu8u" className="card" frameBorder="0" scrolling="no"></iframe>
             <iframe src="https://anchor.fm/devmappodcast/embed/episodes/001---Getting-a-Brown-Taco-e2bo4m" className="card" frameBorder="0" scrolling="no"></iframe>
             <iframe src="https://anchor.fm/devmappodcast/embed/episodes/000---Pilot-e2aljl/a-a5jass" className="card" frameBorder="0" scrolling="no"></iframe>
